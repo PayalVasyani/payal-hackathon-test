da55febb67e423ef33d4413d55d2a97723908b01
Payal <payal@mehtasoftech.com>
2026-08-25 03:30:42 +0530

    feat: initialize environment configuration and bootstrap NestJS application with CORS and global middleware

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -5,17 +5,18 @@ import { AllExceptionsFilter } from './common/filters/all-exceptions.filter';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  
+
   // Enable CORS
   app.enableCors({
-    origin: process.env.FRONTEND_URL || 'http://localhost:4200',
+    origin: [process.env.FRONTEND_URL || 'http://localhost:4200',
+      'https://payal-hackathon-test-5osdql0wv-payals-projects-73b17196.vercel.app/'],
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
     allowedHeaders: 'Content-Type, Authorization, Accept',
   });
-  
+
   // Basic Logging
   const logger = new Logger('Bootstrap');
-  
+
   // Global Validation
   app.useGlobalPipes(new ValidationPipe({
     whitelist: true,
--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -2,5 +2,5 @@ export const environment = {
   production: false,
   supabaseUrl: 'https://burofrrnbxtloxazvqjw.supabase.co',
   supabaseKey: 'sb_publishable_MkEyCQi3pDmQtgt_mNwTkQ_X5t5Itfn',
-  apiUrl: 'http://localhost:3000'
+  apiUrl: 'https://payal-hackathon-test-2.onrender.com'
 };
